36b30cbe1fe2f9826516da0e0d9bb9e52caf408c
elot-idk <elliottwhang2000@gmail.com>
2024-08-12 20:10:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 st.title("Uber Pickups in NYC-elliott")
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text('Loading data...done!')
