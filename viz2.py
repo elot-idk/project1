4797d7a4074800b83a18207b9d104571cbf5f414
elot-idk <elliottwhang2000@gmail.com>
2024-08-19 19:38:58 -0400

    Update viz2.py

--- viz2.py
+++ viz2.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.title('Simple Data Dashboard-Elliott')
 
-DATA_URL = ()
+DATA_URL = ('sales_data.csv')
 def load_data():
   data = pd.read_csv(DATA_URL)
   return data
@@ -12,5 +12,15 @@ def load_data():
 data_load_state = st.text('Loading data...')
 data = load_data()
 data_load_state.text('Loading data...done!')
+
+
+if st.checkbox('Show raw data'):
+  st.subheader('Raw data')
+  st.write(data)
+
+st.subheader('Number of Sales by product')
+hist_values = np.histogram(
+  data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
 
 
