bc772e3e61fab0371de06db8fc706abdb5ac85e1
elot-idk <elliottwhang2000@gmail.com>
2024-08-19 19:44:06 -0400

    Update viz2.py

--- viz2.py
+++ viz2.py
@@ -20,7 +20,5 @@ if st.checkbox('Show raw data'):
 
 st.subheader('Number of Sales by product')
 hist_values = np.histogram(
-  data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+  data.dt.Product, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
-
-
