dea801002299b275be60270f48c8e61efb2ae213
elot-idk <elliottwhang2000@gmail.com>
2024-08-19 19:29:28 -0400

    Update viz2.py

--- viz2.py
+++ viz2.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Simple Data Dashboard-Elliott')
+
+DATA_URL = ()
+def load_data():
+  data = pd.read_csv(DATA_URL)
+  return data
+
+data_load_state = st.text('Loading data...')
+data = load_data()
+data_load_state.text('Loading data...done!')
+
 
